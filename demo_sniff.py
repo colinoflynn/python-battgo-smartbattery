3b713e2622e74824f7624ae7e8b3632be17250df
Colin O'Flynn <colin@oflynn.com>
2026-01-27 20:17:04 -0400

    Make demo sniffer print raw packets as well, useful for sniffing

--- demo_sniff.py
+++ demo_sniff.py
@@ -18,13 +18,16 @@ with serial.Serial('com10', 9600, timeout=1) as ser:
         #    print(out, flush=True)
 
         for p in out:
-            decoded = bgdec.process_packet(p.payload)
-            print(decoded, flush=True)
+            if len(p.payload) > 0:
+                print("%02x %02x %d :"%(p.addr_source, p.addr_dest, p.checksum_ok) + p.payload.hex())
 
-            if old_cellv != bgdec.data.cell_voltage_v:
-                log.write(", ".join(["%f"%v for v in bgdec.data.cell_voltage_v]) + ", %d"%time.time() + " \n")
-                log.flush()
+                decoded = bgdec.process_packet(p.payload)
+                print(decoded, flush=True)
 
-            old_cellv = bgdec.data.cell_voltage_v[:]
+                if old_cellv != bgdec.data.cell_voltage_v:
+                    log.write(", ".join(["%f"%v for v in bgdec.data.cell_voltage_v]) + ", %d"%time.time() + " \n")
+                    log.flush()
+
+                old_cellv = bgdec.data.cell_voltage_v[:]
 
 log.close()
